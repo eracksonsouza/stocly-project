e642a9a57732287934a72de90bfcfa63b59a067e
Erackson Souza <souza.erackson@gmail.com>
2025-05-13 15:17:48 -0300

    feat: implement DataTable and Badge components, enhance product management UI, and update Prisma schema for Windows compatibility

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,33 @@
-const ProductPage = () => {
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { productsTableColumns } from "./_components/table-columns";
+
+const ProductPage = async () => {
+  const products = await db.product.findMany({});
   return (
-    <div>
-      <h1>Product Page</h1>
-      <p>aqui fica o children</p>
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      {/* esquerda */}
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo Produto
+        </Button>
+      </div>
+      {/* tabela */}
+      <DataTable columns={productsTableColumns} data={products} />
+
+      {/* direita */}
+      <div></div>
     </div>
   );
-}
+};
 
 export default ProductPage;
